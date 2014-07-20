be4481087eea72f17e08d8834652ea5eddaef8da
Edwin <edwin@shockoe.com>
2014-07-20 18:04:04 -0400

    Updated lab 4 and 5

--- labcode/Finished_code/05_inputnav/app/controllers/components.js
+++ labcode/Finished_code/05_inputnav/app/controllers/components.js
@@ -1,14 +1,17 @@
 var args = arguments[0] || {};
 
 $.aSwitch.addEventListener('change', function(e) {
+	$.values.text = 'Switch is ' + ((e.value) ? 'on' : 'off');
     Ti.API.info('Switch is ' + ((e.value) ? 'on' : 'off'));
 });
 
-$.aSlider.addEventListener('change', function(e) {
-    // if using Ti SDK 3.1+, the better event to listen for is 'stop'
+$.aSlider.addEventListener('stop', function(e) {
+	// you can also listen for the "change" event
+    $.values.text = 'Slider value is ' + e.value;
     Ti.API.info('Slider value is ' + e.value);
 });
 
 $.aPicker.addEventListener('change', function(e) {
+	$.values.text = 'Picker value is ' + e.selectedValue;
     Ti.API.info('Picker value is ' + e.selectedValue);
 });
